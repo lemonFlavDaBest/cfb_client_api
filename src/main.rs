41e4ba5c1bd632a699843fcea5807931bb4b348a
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-10 08:08:23 -0700

    add initial code + dependencies
    a

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
+
