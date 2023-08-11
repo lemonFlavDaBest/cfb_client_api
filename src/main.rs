024304aa3c6e0bb49c2bc8237269b9b2afe4790f
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-11 09:56:52 -0700

    Update main.rs
    creating headers and authorization

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ use reqwest::header;
 use dotenv::dotenv;
 use std::collections::HashMap;
 
-dotenv().ok();
-
-let api_key = std::env::var!("CFB_API_KEY").expect("MAILCOACH_API_TOKEN must be set.");
-let mut headers = header::HeaderMap::new();
-let mut auth_value = header::HeaderValue::from_static({});
-auth_value.set_sensitive(true);
-headers.insert(header::AUTHORIZATION, "Bearer {}", auth_value);
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let api_key = std::env::var("CFB_API_KEY").expect("CFB_API_TOKEN must be set.");
+    let bearer_api_key = "Bearer {api_key}";
+    let mut headers = header::HeaderMap::new();
+    let mut auth_value = header::HeaderValue::from_static(bearer_api_key);
+    auth_value.set_sensitive(true);
+    headers.insert(header::AUTHORIZATION, auth_value);
+
     let resp = reqwest::get("https://httpbin.org/ip")
         .await?
         .json::<HashMap<String, String>>()
