9673b65905e4dfb4e82cdb48d66355064900ceff
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-11 09:21:09 -0700

    add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use reqwest::header;
+use dotenv::dotenv;
+use std::collections::HashMap;
+
+dotenv().ok();
+
+let api_key = std::env::var!("CFB_API_KEY").expect("MAILCOACH_API_TOKEN must be set.");
+let mut headers = header::HeaderMap::new();
+let mut auth_value = header::HeaderValue::from_static({});
+auth_value.set_sensitive(true);
+headers.insert(header::AUTHORIZATION, "Bearer {}", auth_value);
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = reqwest::get("https://httpbin.org/ip")
