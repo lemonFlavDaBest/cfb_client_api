81ae52203af31a6c6836a102a867b6432e7db49d
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-14 09:53:12 -0700

    Update calendar_endpoint.rs
    adding diff functions for testing

--- src/endpoints/calendar_endpoint.rs
+++ src/endpoints/calendar_endpoint.rs
@@ -14,18 +14,31 @@ pub struct CalendarResponse {
     //lastGameStart: Option<String>,
 }
 
-
-pub async fn get_calendar(api_client: &ApiClient, year: &str) -> Result<String, Error> {
-    let endpoint = "calendar";
-    let params = [("year", year)];
-    let response: Response = api_client.get_with_params(endpoint, &params).await?;
-    let response_text = response.text().await?;
-    println!("Raw JSON Response: {}", response_text);
+//pub async fn get_calendar(api_client: ApiClient, year: &str) -> Result<String, Error> {
+    //let endpoint = "calendar";
+    //let params = [("year", year)];
+    //let response: Response = api_client.get_with_params(endpoint, api_client, &params).await?;
+    //let response_text = response.text().await?;
+    //println!("Raw JSON Response: {}", response_text);
     //let json_response: CalendarResponse = response.json().await?;
     //Ok(json_response)
-    let json_response: Result<CalendarResponse, serde_json::Error> = serde_json::from_str(&response_text);
-    if let Err(e) = json_response {
-    eprintln!("Deserialization Error: {:?}", e);
-    }
-    Ok(response_text)
-}
+    //let json_response: Result<CalendarResponse, serde_json::Error> = serde_json::from_str(&response_text);
+    //if let Err(e) = json_response {
+    //eprintln!("Deserialization Error: {:?}", e);
+    //}
+    //Ok(response_text)
+    
+//pub async fn get_calendar(api_client: ApiClient, year: &str) -> Result<String, Error> {
+    //let endpoint = "calendar";
+    //let params = [("year", year)];
+    //let response: Response = api_client.get_with_params(endpoint, api_client, &params).await?;
+    //let response_text = response.text().await?;
+    //println!("Raw JSON Response: {}", response_text);
+    //let json_response: CalendarResponse = response.json().await?;
+    //Ok(json_response)
+    //let json_response: Result<CalendarResponse, serde_json::Error> = serde_json::from_str(&response_text);
+    //if let Err(e) = json_response {
+    //eprintln!("Deserialization Error: {:?}", e);
+    //}
+    //Ok(response_text)
+//}
