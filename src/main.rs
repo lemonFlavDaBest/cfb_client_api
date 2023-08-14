c3b43048e001e285fbcd2a01909836ce00b7a5ce
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-14 09:53:15 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ mod endpoints;
 
 use dotenv::dotenv;
 use api_client::ApiClient;
+use reqwest::Response;
 //use serde::Deserialize; 
-use endpoints::calendar_endpoint::get_calendar;
+//use endpoints::calendar_endpoint::get_calendar;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,10 +14,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api_key = std::env::var("CFB_API_KEY").expect("CFB_API_KEY must be set.");
     let api_client = ApiClient::new(&api_key)?;
 
-    let year = "2022"; // Set the year for the query
+    // let year = "2022";  Set the year for the query
+    println!("Client: {:?}", api_client);
+    
+    let resp: Response = api_client.client
+        .get("https://api.collegefootballdata.com/calendar")
+        .send()
+        .await?;
 
-    let calendar_response = get_calendar(&api_client, year).await?;
-    println!("{:#?}", calendar_response);
+    println!("{:#?}", resp);
+    let response_text = resp.text().await?;
+    println!("Response Text: {}", response_text);
+
+    
 
     Ok(())
 }
