9b1182de5b2815419976e827c099f1c195d187c5
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-14 12:02:22 -0700

    Update main.rs
    remove the general

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let year = "2022";  Set the year for the query
     println!("Client: {:?}", api_client);
     
-    let resp: Response = api_client.client
-        .get("https://api.collegefootballdata.com/calendar?year=2022")
-        .send()
-        .await?;
-
-    println!("{:#?}", resp);
-    let response_text = resp.text().await?;
-    println!("Response Text: {}", response_text);
-
-    
 
     Ok(())
 }
