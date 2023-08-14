fefcbc66e7b03378b4ab5909c8d694af8705688b
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-14 08:41:24 -0700

    Update api_client.rs
    starting edits. gotta go for walk

--- src/api_client.rs
+++ src/api_client.rs
@@ -1,4 +1,4 @@
-use reqwest::{Client, ClientBuilder, Error, Response};
+use reqwest::{Client, ClientBuilder, Error, Response, Request, RequestBuilder};
 use reqwest::header;
 
 pub struct ApiClient {
@@ -27,16 +27,20 @@ impl ApiClient {
         Ok(res)
     }
 
-    pub async fn get_with_params(&self, endpoint: &str, params: &[(&str, &str)]) -> Result<Response, Error> {
+    pub async fn get_with_params(&self, endpoint: &str, client: ApiClient, params: &[(&str, &str)]) -> Result<Response, Error> {
         
         let url = format!("{}{}", self.base_url, endpoint);
-        let mut request = self.client.get(&url);
+
+        let mut request = Request::new(reqwest::Method::GET, url.parse().unwrap());
+        let request_build: RequestBuilder = RequestBuilder::from_parts(client.client, request);
         
+        println!("request: {:?}", request);
+
         for (key, value) in params {
-            request = request.query(&[(key, value)]);
+            request_build = request_build.query(&[(key, value)]);
         }
 
-        let res = request.send().await?;
+        let res = request_build.send().await?;
         Ok(res)
     }
 }
