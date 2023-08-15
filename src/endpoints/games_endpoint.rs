89f71fdc6fb3355dd861aeddcba1ac986ebdca27
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-15 08:20:59 -0700

    Update games_endpoint.rs
    adding gamesparams struct and implemented new default for gamesparams

--- src/endpoints/games_endpoint.rs
+++ src/endpoints/games_endpoint.rs
@@ -14,15 +14,41 @@ pub struct GamesResponse {
 
 }
 
-pub struct GamesParams {
+#[derive(Debug)]
+pub struct GamesParams<'a> {
+    year: &'a str,
+    week: Option<&'a str>,
+    seasonType: Option<&'a str>,
+    team: Option<&'a str>,
+    home: Option<&'a str>,
+    away: Option<&'a str>,
+    conference: Option<&'a str>,
+    division: Option<&'a str>, //Division classification filter (fbs/fcs/ii/iii)
+    id: Option<&'a str>, // id filter for querying a single game
+}
 
+impl GamesParams<'_> {
+    fn new(year: &str) -> Self {
+        Default::default(year)
+    }
+}
+
+impl Default for GamesParams<'_> {
+    fn default(year: &str) -> Self {
+        GamesParams { 
+            year: year, 
+            week: None, 
+            seasonType: Some("regular"),
+            team: None,
+            home: None,
+            away: None,
+            conference: None,
+            division: None,
+            id: None,
+        } 
+    }
 }
 
 //create function to get games that take the api client as a parameter and an optional parameters struct
-pub async fn get_games_with_params(api_client: &ApiClient, params: Option<GamesParams>) -> Result<Response, Error> {
-    //create the url
-    
-
-    //return the response
-    Ok(response)
+pub async fn get_games_with_params(api_client: &ApiClient, year: &str, params: Option<GamesParams>) -> Result<GamesResponseResponse, Error> {
 }
