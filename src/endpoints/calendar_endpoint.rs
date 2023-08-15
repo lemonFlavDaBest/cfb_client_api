b60724a7073bedf5e4ae0c65b56d84efb9e31469
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-15 10:24:09 -0700

    Update calendar_endpoint.rs
    updated function to now use vecs

--- src/endpoints/calendar_endpoint.rs
+++ src/endpoints/calendar_endpoint.rs
@@ -17,25 +17,27 @@ pub struct CalendarResponse {
 //define the calendar endpoint
 const CALENDAR_ENDPOINT: &str = "calendar";
 
-//pub async fn get_calendar(api_client: ApiClient, year: &str) -> Result<String, Error> {
-    //let endpoint = "calendar";
-    //let params = [("year", year)];
-    //let response: Response = api_client.get_with_params(endpoint, api_client, &params).await?;
-    //let response_text = response.text().await?;
-    //println!("Raw JSON Response: {}", response_text);
-    //let json_response: CalendarResponse = response.json().await?;
-    //Ok(json_response)
-    //let json_response: Result<CalendarResponse, serde_json::Error> = serde_json::from_str(&response_text);
-    //if let Err(e) = json_response {
-    //eprintln!("Deserialization Error: {:?}", e);
-    //}
-    //Ok(response_text)
+
     
     //need to evewntuallu change to CalendarResponse as the result
+    //pub async fn get_calendar(api_client: &ApiClient, year: &str) -> Result<Vec<CalendarResponse>, Error> {
+    //    let params: [(&str, &str); 1] = [("year", year)];
+    //    println!("Params: {:?}", params);
+    //    let response = api_client.get_endpoint_with_params(CALENDAR_ENDPOINT, &params).await?;
+    //    println!("checkpoint");
+    
+        // Parse the response into JSON
+     //   let json_response: Vec<CalendarResponse> = response.json().await?;
+    
+    //    println!("JSON Response: {:?}", json_response);
+    //    Ok(json_response)
+    //}
+
     pub async fn get_calendar(api_client: &ApiClient, year: &str) -> Result<Vec<CalendarResponse>, Error> {
-        let params: [(&str, &str); 1] = [("year", year)];
+        let params: Vec<(&str, &str)> = vec![("year", year)];
         println!("Params: {:?}", params);
-        let response = api_client.get_endpoint_with_params(CALENDAR_ENDPOINT, &params).await?;
+        
+        let response = api_client.get_endpoint_with_params(CALENDAR_ENDPOINT, params).await?;
         println!("checkpoint");
     
         // Parse the response into JSON
