5295678545d76eeb5509ae465092ee87e9634959
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-14 17:06:23 -0700

    Update games_endpoint.rs
    add struct outline

--- src/endpoints/games_endpoint.rs
+++ src/endpoints/games_endpoint.rs
@@ -7,3 +7,9 @@ use crate::api_client::ApiClient; // Import the ApiClient from the parent module
 
 //define the endpoint
 const GAMES_ENDPOINT: &str = "games";
+
+//define the response struct
+#[derive(Deserialize, Debug)]
+pub struct GamesResponse {
+
+}
