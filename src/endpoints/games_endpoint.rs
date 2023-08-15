69eab07c4dedbc56f38c94dd3df552a307242d82
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-15 10:07:43 -0700

    Update games_endpoint.rs
    I think we are cooking. added as_query_params impl method. added working function get_games_with_parms

--- src/endpoints/games_endpoint.rs
+++ src/endpoints/games_endpoint.rs
@@ -31,6 +31,36 @@ impl GamesParams<'_> {
     fn new() -> Self {
         Default::default()
     }
+
+    fn as_query_params(&self) -> Vec<(&str, &str)> {
+        let mut params = vec![("year", self.year)];
+        // Add other fields if they exist in self
+        if let Some(week) = self.week {
+            params.push(("week", week));
+        }
+        if let Some(season_type) = self.seasonType {
+            params.push(("seasonType", season_type));
+        }
+        if let Some(team) = self.team {
+            params.push(("team", team));
+        }
+        if let Some(home) = self.home {
+            params.push(("home", home));
+        }
+        if let Some(away) = self.away {
+            params.push(("away", away));
+        }
+        if let Some(conference) = self.conference {
+            params.push(("conference", conference));
+        }
+        if let Some(division) = self.division {
+            params.push(("division", division));
+        }
+        if let Some(id) = self.id {
+            params.push(("id", id));
+        }  
+        params
+    }
 }
 
 impl Default for GamesParams<'_> {
@@ -50,7 +80,15 @@ impl Default for GamesParams<'_> {
 }
 
 //create function to get games that take the api client as a parameter and an optional parameters struct
-pub async fn get_games_with_params(api_client: &ApiClient, year: &str, params: Option<GamesParams>) -> Result<GamesResponseResponse, Error> {
-    let mut games = GamesParams::new();
-    games.year = year;
+pub async fn get_games_with_params(api_client: &ApiClient, year: &str, params: Option<GamesParams<'_>>) -> Result<GamesResponse, Error> {
+    let mut games_params = params.unwrap_or_else(GamesParams::new);
+    games_params.year = year;
+
+    let endpoint = GAMES_ENDPOINT;
+    let response = api_client.get_endpoint_with_params(endpoint, games_params.as_query_params()).await?;
+
+    // Deserialize the response into GamesResponse struct
+    let json_response = response.json::<GamesResponse>().await?;
+
+    Ok(json_response)
 }
