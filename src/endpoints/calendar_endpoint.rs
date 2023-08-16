f6cd88b289a57ae1d75fba1fd088d114e844058d
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-16 15:59:48 -0700

    Update calendar_endpoint.rs
    adding some time stuff

--- src/endpoints/calendar_endpoint.rs
+++ src/endpoints/calendar_endpoint.rs
@@ -1,7 +1,9 @@
 // calendar_endpoint.rs
 
+use chrono::{DateTime, Utc, TimeZone};
 use reqwest::{Error, Response};
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
+use serde_json::Value;
 
 use crate::api_client::ApiClient; // Import the ApiClient from the parent module
 
@@ -10,8 +12,36 @@ pub struct CalendarResponse {
     pub season: String,
     pub week: i32,
     pub seasonType: String,
-    pub firstGameStart: String,
-    pub lastGameStart: String,
+    #[serde(deserialize_with = "deserialize_game_start")]
+    pub firstGameStart: Option<DateTime<Utc>>,
+    #[serde(deserialize_with = "deserialize_game_start")]
+    pub lastGameStart: Option<DateTime<Utc>>,
+}
+
+
+fn deserialize_game_start<'de, D>(deserializer: D) -> Result<Option<DateTime<Utc>>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value: Option<Value> = Option::deserialize(deserializer)?;
+    match value {
+        Some(val) => {
+            let wallclock_str = val
+                .as_str()
+                .ok_or_else(|| serde::de::Error::custom("Invalid wallclock format"))?;
+
+            // Define a custom date and time format
+            let custom_format = "%Y-%m-%dT%H:%M:%S%.3fZ";
+
+            // Parse the wallclock string using the custom format
+            let parsed_datetime = Utc
+                .datetime_from_str(wallclock_str, custom_format)
+                .map_err(|_err| serde::de::Error::custom("Failed to parse wallclock datetime"))?;
+
+            Ok(Some(parsed_datetime))
+        }
+        None => Ok(None),
+    }
 }
 
 //define the calendar endpoint
