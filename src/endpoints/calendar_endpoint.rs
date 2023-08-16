5cb3c3d5e260447e54e344a3922536ee2ebd5487
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-16 10:57:07 -0700

    Update calendar_endpoint.rs
    re ove commenting

--- src/endpoints/calendar_endpoint.rs
+++ src/endpoints/calendar_endpoint.rs
@@ -19,14 +19,14 @@ const CALENDAR_ENDPOINT: &str = "calendar";
 
     pub async fn get_calendar(api_client: &ApiClient, year: &str) -> Result<Vec<CalendarResponse>, Error> {
         let params: Vec<(&str, &str)> = vec![("year", year)];
-        println!("Params: {:?}", params);
+        //println!("Params: {:?}", params);
         
         let response = api_client.get_endpoint_with_params(CALENDAR_ENDPOINT, params).await?;
-        println!("checkpoint");
+        //println!("checkpoint");
     
         // Parse the response into JSON
         let json_response: Vec<CalendarResponse> = response.json().await?;
     
-        println!("JSON Response: {:?}", json_response);
+        //println!("JSON Response: {:?}", json_response);
         Ok(json_response)
     }
