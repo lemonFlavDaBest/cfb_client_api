a405c3f73ee6995f2052731e178d27b713eb339e
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-24 13:36:27 -0700

    Update scoreboard_endpoint.rs
    add acoreboard endpoint

--- src/endpoints/scoreboard_endpoint.rs
+++ src/endpoints/scoreboard_endpoint.rs
@@ -7,26 +7,28 @@ use serde_json::{Value};
 use std::str::FromStr;
 use tqdm::tqdm;
 
+use crate::api_client::ApiClient; 
+
 const SCOREBOARD_ENDPOINT: &str = "scoreboard";
 
 #[derive(Debug, serde::Deserialize)]
 pub struct ScoreboardResponse {
-    id: Option<u64>,
-    startDate: Option<String>,
-    startTimeTBD: Option<bool>,
-    tv: Option<String>,
-    neutralSite: Option<bool>,
-    conferenceGame: Option<bool>,
-    status: Option<String>,
-    period: Option<u8>,
-    clock: Option<String>,
-    situation: Option<String>,
-    possession: Option<String>,
-    venue: Option<Venue>,
-    homeTeam: Option<Team>,
-    awayTeam: Option<Team>,
-    weather: Option<Weather>,
-    betting: Option<Betting>,
+    //id: Option<u64>,
+    //startDate: Option<String>,
+    //startTimeTBD: Option<bool>,
+    //tv: Option<String>,
+    //neutralSite: Option<bool>,
+    //conferenceGame: Option<bool>,
+    //status: Option<String>,
+    //period: Option<u8>,
+    //clock: Option<String>,
+    //situation: Option<String>,
+    //possession: Option<String>,
+    //venue: Option<Venue>,
+    //homeTeam: Option<Team>,
+    //awayTeam: Option<Team>,
+    //weather: Option<Weather>,
+    //betting: Option<Betting>,
 }
 
 #[derive(Debug, serde::Deserialize)]
@@ -61,4 +63,16 @@ pub struct Betting {
     awayMoneyline: Option<i32>,
 }
 
+
+pub async fn get_scoreboard(api_client: &ApiClient) -> Result<Vec<ScoreboardResponse>, Error> {
+
+    let response = api_client.get_endpoint(SCOREBOARD_ENDPOINT,).await?;
+    //println!("checkpoint");
+
+    // Parse the response into JSON
+    let json_response: Vec<ScoreboardResponse> = response.json().await?;
+
+    //println!("JSON Response: {:?}", json_response);
+    Ok(json_response)
+}
 
