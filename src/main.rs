bb3943335b5e358072d6e1154c2b0f52b32d9219
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-24 14:42:29 -0700

    Update main.rs
    add scoreboard endpoitn structs and functions

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use api_client::ApiClient;
 use endpoints::calendar_endpoint::{get_calendar, CalendarResponse};
 use endpoints::games_endpoint::{get_games_with_params, GamesResponse, GamesParams};
 use endpoints::plays_endpoint::{get_plays_with_params, get_all_plays_for_year_range, PlaysResponse};
+use endpoints::scoreboard_endpoint::{get_scoreboard, ScoreboardResponse};
 use polars::prelude::*;
 use reqwest::Error;
 use std::fs::File;
