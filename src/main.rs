eb985d3eee28bc28afe0209b9a1cabd99692660b
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-26 07:38:30 -0700

    Update main.rs
    add scripting

--- src/main.rs
+++ src/main.rs
@@ -55,28 +55,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //testing get_all_plays_for_year_range here
     //ready tot test tomorrows
     {
-        let start_year = 2016;
-        let end_year = 2027;
-        let plays_data = get_all_plays_for_year_range(&api_client, start_year, end_year, 1, 15).await?;
+        //let start_year = 2016;
+        //let end_year = 2027;
+        //let plays_data = get_all_plays_for_year_range(&api_client, start_year, end_year, 1, 15).await?;
         //println!("{:#?}", response);
         // Create a DataFrame from the plays_data
-        let json_string = serde_json::to_string(&plays_data)?;
+        //let json_string = serde_json::to_string(&plays_data)?;
 
         // Specify the path and filename to save the JSON file
-        let filename = format!("plays_response_{}_{}.json", start_year, end_year);
+        //let filename = format!("plays_response_{}_{}.json", start_year, end_year);
 
         // Create or open the file for writing
-        let mut file = File::create(&filename)?;
+        //let mut file = File::create(&filename)?;
 
         // Write the JSON string to the file
-        file.write_all(json_string.as_bytes())?;
+        //file.write_all(json_string.as_bytes())?;
     
         // Print a success message
-        println!("JSON response saved as: {}", filename);
+        //println!("JSON response saved as: {}", filename);
     
     
     }
-    
+
+    //testing the scoreboard endpoint here
+    {
+        let scoreboard_response = get_scoreboard(&api_client).await?;
+        println!("{:#?}", scoreboard_response);
+    }
     Ok(())
 
 }
