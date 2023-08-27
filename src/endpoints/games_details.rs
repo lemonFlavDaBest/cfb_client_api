2fc52de311fb70d95e4b169aae692698066b9278
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-27 11:21:04 -0700

    Update games_details.rs
    add getting game weather information

--- src/endpoints/games_details.rs
+++ src/endpoints/games_details.rs
@@ -9,13 +9,14 @@ use crate::api_client::ApiClient; // Import the ApiClient from the parent module
 //define the endpoint
 const GAMES_ENDPOINT: &str = "games";
 const MEDIA_ENDPOINT: &str = "media";
+const WEATHER_ENDPOINT: &str = "weather";
 
 
 pub struct MediaParams<'a> {
-    year: &'a str,
-    week: Option<&'a str>,
+    pub year: &'a str,
+    pub week: Option<&'a str>,
     pub seasonType: Option<&'a str>,
-    team: Option<&'a str>,
+    pub team: Option<&'a str>,
     pub conference: Option<&'a str>,
     pub mediaType: Option<&'a str>, 
     pub classification: Option<&'a str>, 
@@ -52,6 +53,7 @@ impl MediaParams<'_> {
 }
 
 impl Default for MediaParams<'_> {
+
     fn default() -> Self {
         MediaParams { 
             year: "2023", 
@@ -65,12 +67,84 @@ impl Default for MediaParams<'_> {
     }
 }
 
+pub struct WeatherParams<'a> {
+    pub gameId: Option<&'a str>,
+    pub year: Option<&'a str>,
+    pub week: Option<&'a str>,
+    pub seasonType: Option<&'a str>,
+    pub team: Option<&'a str>,
+    pub conference: Option<&'a str>,
+    pub classification: Option<&'a str>, 
+}
 
-pub async fn get_games_media_with_params(api_client: &ApiClient, year: &str, params: Option<GamesParams<'_>>) -> Result<Vec<GamesResponse>, Error> {
-    let mut games_params = params.unwrap_or_else(GamesParams::new);
+impl WeatherParams<'_> {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    fn as_query_params(&self) -> Vec<(&str, &str)> {
+        let mut params = vec![("year", self.year)];
+        // Add other fields if they exist in self
+        if let Some(week) = self.week {
+            params.push(("week", week));
+        }
+        if let Some(season_type) = self.seasonType {
+            params.push(("seasonType", season_type));
+        }
+        if let Some(team) = self.team {
+            params.push(("team", team));
+        }
+        if let Some(conference) = self.conference {
+            params.push(("conference", conference));
+        }
+        if let Some(game_id) = self.gameId {
+            params.push(("gameId", game_id));
+        }
+        if let Some(classification) = self.classification {
+            params.push(("classification", classification));
+        }  
+        params
+    }
+}
+
+impl Default for WeatherParams<'_> {
+
+    fn default() -> Self {
+        WeatherParams { 
+            year: "2023", 
+            week: None, 
+            seasonType: Some("regular"),
+            team: None,
+            conference: None,
+            gameId: None,
+            classification: None,
+        } 
+    }
+}
+
+pub async fn get_games_media_with_params(api_client: &ApiClient, year: &str, params: Option<MediaParams<'_>>) -> Result<Vec<GamesResponse>, Error> {
+    let mut games_params = params.unwrap_or_else(MediaParams::new);
     games_params.year = year;
 
     let endpoint = format!("{}/{}", GAMES_ENDPOINT, MEDIA_ENDPOINT);
+    let response = api_client.get_endpoint_with_params(&endpoint, games_params.as_query_params()).await?;
+    //println!("checkpoint");
+    //print response as text
+   
+    //Ok(response.text().await?)
+
+    //Deserialize the response into GamesResponse struct
+    let json_response: Vec<GamesResponse> = response.json().await?;
+    //println!("json_response: {:?}", json_response);
+
+    Ok(json_response)
+}
+
+pub async fn get_games_weather_with_params(api_client: &ApiClient, year: &str, params: Option<WeatherParams<'_>>) -> Result<Vec<GamesResponse>, Error> {
+    let mut games_params = params.unwrap_or_else(WeatherParams::new);
+    games_params.year = year;
+
+    let endpoint = format!("{}/{}", GAMES_ENDPOINT, WEATHER_ENDPOINT);
     let response = api_client.get_endpoint_with_params(&endpoint, games_params.as_query_params()).await?;
     //println!("checkpoint");
     //print response as text
