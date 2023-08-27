987221ddda1f7980b997f0ec5b233c341803c572
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-27 11:31:53 -0700

    Update games_details.rs
    add players detaials endpoint and change some of the seasont ytpes around

--- src/endpoints/games_details.rs
+++ src/endpoints/games_details.rs
@@ -10,6 +10,7 @@ use crate::api_client::ApiClient; // Import the ApiClient from the parent module
 const GAMES_ENDPOINT: &str = "games";
 const MEDIA_ENDPOINT: &str = "media";
 const WEATHER_ENDPOINT: &str = "weather";
+const PLAYERS_ENDPOINT: &str = "players";
 
 
 pub struct MediaParams<'a> {
@@ -62,7 +63,7 @@ impl Default for MediaParams<'_> {
         MediaParams { 
             year: "2023", 
             week: None, 
-            seasonType: Some("regular"),
+            seasonType: Some("both"),
             team: None,
             conference: None,
             mediaType: None,
@@ -117,7 +118,7 @@ impl Default for WeatherParams<'_> {
         WeatherParams { 
             year: "2023", 
             week: None, 
-            seasonType: Some("regular"),
+            seasonType: Some("both"),
             team: None,
             conference: None,
             gameId: None,
@@ -127,6 +128,63 @@ impl Default for WeatherParams<'_> {
 }
 
 pub struct WeatherResponse {}
+
+pub struct PlayersParams<'a> {
+    pub year: &'a str,
+    pub week: Option<&'a str>,
+    pub seasonType: Option<&'a str>,
+    pub team: Option<&'a str>,
+    pub conference: Option<&'a str>,
+    pub category: Option<&'a str>, 
+    pub gameId: Option<&'a str>,
+}
+
+pub struct PlayersResponse {}
+
+impl PlayersParams<'_> {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    fn as_query_params(&self) -> Vec<(&str, &str)> {
+        let mut params = vec![("year", self.year)];
+        // Add other fields if they exist in self
+        if let Some(week) = self.week {
+            params.push(("week", week));
+        }
+        if let Some(season_type) = self.seasonType {
+            params.push(("seasonType", season_type));
+        }
+        if let Some(team) = self.team {
+            params.push(("team", team));
+        }
+        if let Some(conference) = self.conference {
+            params.push(("conference", conference));
+        }
+        if let Some(category) = self.category {
+            params.push(("category", category));
+        }
+        if let Some(game_id) = self.gameId {
+            params.push(("gameId", game_id));
+        }
+        params
+    }
+
+}
+
+impl Default for PlayersParams<'_> {
+    fn default() -> Self {
+        PlayersParams { 
+            year: "2023", 
+            week: None, 
+            seasonType: Some("regular"),
+            team: None,
+            conference: None,
+            category: None,
+            gameId: None,
+        } 
+    }
+}
 
 pub async fn get_games_media_with_params(api_client: &ApiClient, year: &str, params: Option<MediaParams<'_>>) -> Result<Vec<MediaResponse>, Error> {
     let mut games_params = params.unwrap_or_else(MediaParams::new);
