b2659fb9c5650aff0e19a3755bfe3f517952b002
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-28 11:39:01 -0700

    Update plays_details.rs
    add get live plays

--- src/endpoints/plays_details.rs
+++ src/endpoints/plays_details.rs
@@ -16,3 +16,23 @@ const LIVE_ENDPOINT: &str = "live";
 const TYPES_ENDPOINT: &str = "types";
 const STATS_ENDPOINT: &str = "stats";
 const STAT_ENDPOINT: &str = "stat";
+
+pub struct LivePlayParams {
+    gameId: &'a str,
+}
+
+pub struct LivePlayResponse{}
+
+pub async fn get_live_plays(api_client: &ApiClient, game_id: &str) -> Result<Vec<LivePlayResponse>, Error> {
+    let params: Vec<(&str, &str)> = vec![("gameId", game_id)];
+    //println!("Params: {:?}", params);
+    
+    let response = api_client.get_endpoint_with_params(LIVE_ENDPOINT, params).await?;
+    //println!("checkpoint");
+    
+    // Parse the response into JSON
+    let json_response: Vec<LivePlayResponse> = response.json().await?;
+    
+    //println!("JSON Response: {:?}", json_response);
+    Ok(json_response)
+}
