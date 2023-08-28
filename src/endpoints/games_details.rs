754b734cd11a6dcfdba17214e3c52136da047a81
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-28 07:38:40 -0700

    Update games_details.rs
    add the teams details for the game details

--- src/endpoints/games_details.rs
+++ src/endpoints/games_details.rs
@@ -196,6 +196,44 @@ pub struct TeamsParams<'a> {
     classification: Option<&'a str>,
 }
 pub struct TeamsResponse {}
+impl TeamsParams {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    fn as_query_params(&self) -> Vec<(&str, &str)> {
+        let mut params = vec![("year", self.year)];
+        // Add other fields if they exist in self
+        if let Some(week) = self.week {
+            params.push(("week", week));
+        }
+        if let Some(season_type) = self.seasonType {
+            params.push(("seasonType", season_type));
+        }
+        if let Some(team) = self.team {
+            params.push(("team", team));
+        }
+        if let Some(conference) = self.conference {
+            params.push(("conference", conference));
+        }
+        if let Some(classification) = self.classification {
+            params.push(("classification", classification));
+        }  
+        params
+    }
+}
+impl Default for TeamsParams {
+    fn default() -> Self {
+        TeamsParams { 
+            year: "2023", 
+            week: None, 
+            seasonType: Some("regular"),
+            team: None,
+            conference: None,
+            classification: None,
+        } 
+    }
+}
 
 pub async fn get_games_media_with_params(api_client: &ApiClient, year: &str, params: Option<MediaParams<'_>>) -> Result<Vec<MediaResponse>, Error> {
     let mut games_params = params.unwrap_or_else(MediaParams::new);
@@ -228,6 +266,24 @@ pub async fn get_games_weather_with_params(api_client: &ApiClient, year: &str, p
 
     //Deserialize the response into GamesResponse struct
     let json_response: Vec<WeatherResponse> = response.json().await?;
+    //println!("json_response: {:?}", json_response);
+
+    Ok(json_response)
+}
+
+pub async fn get_games_teams_with_params(api_client: &ApiClient, year: &str, params: Option<TeamsParams<'_>>) -> Result<Vec<TeamsResponse>, Error> {
+    let mut games_params = params.unwrap_or_else(TeamsParams::new);
+    games_params.year = year;
+
+    let endpoint = format!("{}/{}", GAMES_ENDPOINT, TEAMS_ENDPOINT);
+    let response = api_client.get_endpoint_with_params(&endpoint, games_params.as_query_params()).await?;
+    //println!("checkpoint");
+    //print response as text
+   
+    //Ok(response.text().await?)
+
+    //Deserialize the response into GamesResponse struct
+    let json_response: Vec<TeamsResponse> = response.json().await?;
     //println!("json_response: {:?}", json_response);
 
     Ok(json_response)
