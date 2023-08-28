b2fe462890b3f2d9800e3b5f2e6a6eb68d0e8a58
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-28 11:26:30 -0700

    Update plays_details.rs
    getting the imports and setting up the endpoitns

--- src/endpoints/plays_details.rs
+++ src/endpoints/plays_details.rs
@@ -1,1 +1,15 @@
 //plays_details.rs
+
+
+use reqwest::{Error, Response};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use chrono::{DateTime, Utc, TimeZone, NaiveDateTime};
+use serde_json::{Value};
+use std::str::FromStr;
+use tqdm::tqdm;
+
+use crate::api_client::ApiClient; // Import the ApiClient from the parent module
+
+//define the endpoint
+const PLAYS_ENDPOINT: &str = "plays";
+const LIVE_ENDPOINT: &str = "live";
