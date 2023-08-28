4096e0e76178bf0a0ea0772092906309a27c4f14
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-28 09:11:24 -0700

    Update drives_endpoint.rs
    add drives endpoint function and structures

--- src/endpoints/drives_endpoint.rs
+++ src/endpoints/drives_endpoint.rs
@@ -9,3 +9,89 @@ use crate::api_client::ApiClient; // Import the ApiClient from the parent module
 //define the endpoint
 const DRIVES_ENDPOINT: &str = "drives";
 
+//define the response struct
+#[derive(Deserialize, Debug)]
+pub struct DrivesResponse {
+
+}
+
+//define the params struct
+pub struct DrivesParams<'a> {
+    year: &'a str,
+    team: Option<&'a str>,
+    week: Option<&'a str>,
+    offense: Option<&'a str>,
+    defense: Option<&'a str>,
+    conference: Option<&'a str>,
+    offenseConference: Option<&'a str>,
+    defenseConference: Option<&'a str>,
+    classification: Option<&'a str>,
+}
+
+impl DrivesParams<'_> {
+    pub fn new() -> DrivesParams {
+        Default::default()
+    }
+
+    fn as_query_params(&self) -> Vec<(&str, &str)> {
+        let mut params = Vec::new();
+        // Add other fields if they exist in self
+        if let Some(year) = self.year {
+            params.push(("year", year));
+        }
+        if let Some(team) = self.team {
+            params.push(("team", team));
+        }
+        if let Some(week) = self.week {
+            params.push(("week", week));
+        }
+        if let Some(offense) = self.offense {
+            params.push(("offense", offense));
+        }
+        if let Some(defense) = self.defense {
+            params.push(("defense", defense));
+        }
+        if let Some(conference) = self.conference {
+            params.push(("conference", conference));
+        }
+        if let Some(offenseConference) = self.offenseConference {
+            params.push(("offenseConference", offenseConference));
+        }
+        if let Some(defenseConference) = self.defenseConference {
+            params.push(("defenseConference", defenseConference));
+        }
+        if let Some(classification) = self.classification {
+            params.push(("classification", classification));
+        }
+        params
+    }
+}
+
+impl Default for DrivesParams<'_> {
+    fn default() -> Self {
+        DrivesParams { 
+            year: "2023", 
+            team: None,
+            week: None,
+            offense: None,
+            defense: None,
+            conference: None,
+            offenseConference: None,
+            defenseConference: None,
+            classification: None,
+        }
+    }
+}
+
+pub async fn get_drives_with_params(api_client: &ApiClient, params: Option<DrivesParams<'_>>) -> Result<Vec<DrivesResponse>, Error> {
+    let mut drives_params = params.unwrap_or_else(DrivesParams::new);
+    let endpoint = DRIVES_ENDPOINT;
+    let response = api_client.get_endpoint_with_params(endpoint, drives_params.as_query_params()).await?;
+    //println!("checkpoint");
+    
+    // Parse the response into JSON
+    let json_response: Vec<DrivesResponse> = response.json().await?;
+    
+    //println!("JSON Response: {:?}", json_response);
+    Ok(json_response)
+}
