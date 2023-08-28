f796fb67ebe69ee9fa06089e06d8e593f36ca640
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-28 08:39:08 -0700

    Update drives_endpoint.rs
    add templating

--- src/endpoints/drives_endpoint.rs
+++ src/endpoints/drives_endpoint.rs
@@ -1,1 +1,11 @@
 //drives_endpoint.rs
+use chrono::{DateTime, Utc, TimeZone};
+use reqwest::{Error, Response};
+use serde::{Deserialize, Deserializer, de};
+use serde_json::Value;
+
+use crate::api_client::ApiClient; // Import the ApiClient from the parent module
+
+//define the endpoint
+const DRIVES_ENDPOINT: &str = "drives";
+
