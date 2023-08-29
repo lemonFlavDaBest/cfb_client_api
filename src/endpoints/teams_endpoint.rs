d90c5f49cbd9ba52050ef91338ebab0a8ac2c308
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-29 14:34:18 -0700

    Update teams_endpoint.rs
    adding get teams with optional parmas

--- src/endpoints/teams_endpoint.rs
+++ src/endpoints/teams_endpoint.rs
@@ -6,3 +6,47 @@ use serde::{Deserialize, Deserializer, de};
 use serde_json::Value;
 
 use crate::api_client::ApiClient; // Import the ApiClient from the parent module
+
+//define the endpoint
+const TEAMS_ENDPOINT: &str = "teams";
+
+pub struct TeamsParams<'a> {
+    conference: Option<&'a str>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct TeamsResponse {}
+
+impl TeamsParams<'_> {
+    fn as_query_params(&self) -> Vec<(&str, &str)> {
+        let mut params = Vec::new();
+        // Add other fields if they exist in self
+        if let Some(conference) = self.conference {
+            params.push(("conference", conference));
+        }
+        params
+    }
+}
+
+pub async fn get_teams(api_client: &ApiClient, params: Option<TeamsParams<'_>>) -> Result<Vec<TeamsResponse>, Error> {
+    // I want to match params with some and none. if some, then unwrap and call get_endpoint_with_parms.
+    //if none, then just call get_endpoint
+    let teams_response: Vec<TeamsResponse> = match params {
+        Some(params) => {
+            let response = api_client.get_endpoint_with_params(TEAMS_ENDPOINT, params.as_query_params()).await?;
+            //println!("checkpoint");
+            //print response as text
+            
+            response.json().await?
+        },
+        None => {
+            let endpoint = TEAMS_ENDPOINT;
+            let response = api_client.get_endpoint(endpoint).await?;
+            //println!("checkpoint");
+            //print response as text
+            
+            response.json().await?
+        }
+    };
+    Ok(teams_response)
+}
