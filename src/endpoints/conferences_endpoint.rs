ab010b34673aca88454b501eedf48d71bf75f722
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-30 08:45:16 -0700

    Update conferences_endpoint.rs
    adding conferences function and response

--- src/endpoints/conferences_endpoint.rs
+++ src/endpoints/conferences_endpoint.rs
@@ -6,3 +6,16 @@ use serde::{Deserialize, Deserializer, de};
 use serde_json::Value;
 
 use crate::api_client::ApiClient; // Import the ApiClient from the parent module
+
+//define the endpoint
+const CONFERENCES_ENDPOINT: &str = "conferences";
+
+//define the response struct
+#[derive(Deserialize, Debug)]
+pub struct ConferencesResponse {}
+
+async fn get_conferences(api_client: &ApiClient) -> Result<Conferences, Error> {
+    let response = api_client.get_endpoint(CONFERENCES_ENDPOINT).await?;
+    let json_response: ConferencesResponse = response.json().await?;
+    Ok(json_response)
+}
