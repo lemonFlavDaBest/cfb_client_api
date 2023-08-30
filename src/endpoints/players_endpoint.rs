75595ab3769b0188e0b7058af3e5fb630e8e30a1
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-30 15:45:55 -0700

    Update players_endpoint.rs
    add player search structs and functions

--- src/endpoints/players_endpoint.rs
+++ src/endpoints/players_endpoint.rs
@@ -1,6 +1,6 @@
 // players_endpoint.rs
 
-use chrono::{DateTime, Utc, TimeZone};
+use chrono::{DateTime, Utc, TimeZone, format};
 use reqwest::{Error, Response};
 use serde::{Deserialize, Deserializer, de};
 use serde_json::Value;
@@ -16,3 +16,37 @@ const STATS_ENDPOINT: &str = "stats";
 const PORTAL_ENDPOINT: &str = "portal";
 const SEASON_ENDPOINT: &str = "season";
 
+pub struct PlayerSearchParams<'a> {
+    searchTerm: &'a str,
+    position: Option<&'a str>,
+    team: Option<&'a str>,
+    year: Option<&'a str>,
+}
+
+impl PlayerSearchParams<'_> {
+    fn as_query_params(&self) -> Vec<(&str, &str)> {
+        let mut params = Vec::new();
+        params.push(("searchTerm", self.searchTerm));
+        // Add other fields if they exist in self
+        if let Some(position) = self.position {
+            params.push(("position", position));
+        }
+        if let Some(team) = self.team {
+            params.push(("team", team));
+        }
+        if let Some(year) = self.year {
+            params.push(("year", year));
+        }
+        params
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PlayerSearchResponse {}
+
+pub async fn get_player_search_with_params(api_client: &ApiClient, params: PlayerSearchParams<'_>) -> Result<PlayerSearchResponse, Error> {
+    let endpoint = format!("{}/{}", PLAYER_ENDPOINT, SEARCH_ENDPOINT);
+    let response = api_client.get_endpoint_with_params(&endpoint, params.as_query_params()).await?;
+    let json_response: PlayerSearchResponse = response.json().await?;
+    Ok(json_response)
+}
