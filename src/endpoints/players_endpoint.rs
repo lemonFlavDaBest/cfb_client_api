3be85af980c1d07084ea7260c2f83f68a833dbb5
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-30 10:46:01 -0700

    Update players_endpoint.rs

--- src/endpoints/players_endpoint.rs
+++ src/endpoints/players_endpoint.rs
@@ -6,3 +6,6 @@ use serde::{Deserialize, Deserializer, de};
 use serde_json::Value;
 
 use crate::api_client::ApiClient; // Import the ApiClient from the parent module
+
+//define the endpoint
+const PLAYER_ENDPOINT: &str = "player";
