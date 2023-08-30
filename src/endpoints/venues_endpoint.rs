a4b0c9c0029168356c414f972c44ad60bcb9cd53
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-30 08:46:15 -0700

    Create venues_endpoint.rs
    create and add imports

---
+++ src/endpoints/venues_endpoint.rs
@@ -0,0 +1,8 @@
+//venues_endpoint.rs
+
+use chrono::{DateTime, Utc, TimeZone};
+use reqwest::{Error, Response};
+use serde::{Deserialize, Deserializer, de};
+use serde_json::Value;
+
+use crate::api_client::ApiClient; // Import the ApiClient from the parent module
