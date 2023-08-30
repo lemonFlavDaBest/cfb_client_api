bf9ed699f46dbcc07cf52ecadd9f4553e3da0ce2
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-30 08:49:00 -0700

    Update venues_endpoint.rs
    add functionality to get venues

--- src/endpoints/venues_endpoint.rs
+++ src/endpoints/venues_endpoint.rs
@@ -9,3 +9,13 @@ use crate::api_client::ApiClient; // Import the ApiClient from the parent module
 
 //define the endpoint
 const VENUES_ENDPOINT: &str = "venues";
+
+//define the response struct
+#[derive(Deserialize, Debug)]
+pub struct VenuesResponse {}
+
+pub async fn get_venues(api_client: &ApiClient) -> Result<VenuesResponse, Error> {
+    let response = api_client.get_endpoint(VENUES_ENDPOINT).await?;
+    let json_response: VenuesResponse = response.json().await?;
+    Ok(json_response)
+}
