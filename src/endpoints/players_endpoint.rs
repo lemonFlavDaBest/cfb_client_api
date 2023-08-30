f298515d76d0a8285780630a3596501274df117a
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-30 14:08:14 -0700

    Update players_endpoint.rs
    add all my endpoints

--- src/endpoints/players_endpoint.rs
+++ src/endpoints/players_endpoint.rs
@@ -9,3 +9,9 @@ use crate::api_client::ApiClient; // Import the ApiClient from the parent module
 
 //define the endpoint
 const PLAYER_ENDPOINT: &str = "player";
+const SEARCH_ENDPOINT: &str = "search";
+const USAGE_ENDPOINT: &str = "usage";
+const RETURNING_ENDPOINT: &str = "returning";
+const STATS_ENDPOINT: &str = "stats";
+const PORTAL_ENDPOINT: &str = "portal";
+const SEASON_ENDPOINT: &str = "season";
