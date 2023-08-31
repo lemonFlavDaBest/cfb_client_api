c45e1ccc180db5dc6e99d5a9c463d50d743feae0
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-31 10:28:22 -0700

    Create ratings_endpoint.rs
    create file, add imports, and endpoint

---
+++ src/endpoints/ratings_endpoint.rs
@@ -0,0 +1,11 @@
+// ratings_endpoint.rs
+
+use chrono::{DateTime, Utc, TimeZone};
+use reqwest::{Error, Response};
+use serde::{Deserialize, Deserializer, de};
+use serde_json::Value;
+
+use crate::api_client::ApiClient; // Import the ApiClient from the parent module
+
+//define the endpoint
+const RATINGS_ENDPOINT: &str = "ratings";
