104bee2e60e44fa27df66d5c6b453ffcedf3dc3a
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-31 09:01:11 -0700

    Update betting_endpoint.rs
    add vetting params struct

--- src/endpoints/betting_endpoint.rs
+++ src/endpoints/betting_endpoint.rs
@@ -6,3 +6,18 @@ use serde::{Deserialize, Deserializer, de};
 use serde_json::Value;
 
 use crate::api_client::ApiClient; // Import the ApiClient from the parent module
+
+//define the endpoint
+const BETTING_ENDPOINT: &str = "betting";
+
+//define the params struct
+pub struct BettingParams<'a> {
+    gameId: Option<&'a str>,
+    year: Option<&'a str>,
+    week: Option<&'a str>,
+    seasonType: Option<&'a str>,
+    team: Option<&'a str>,
+    conference: Option<&'a str>,
+    home: Option<&'a str>,
+    away: Option<&'a str>,
+}
