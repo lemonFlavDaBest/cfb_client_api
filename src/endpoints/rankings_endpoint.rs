5922ffd3d4b2c00c47c400bdb3d2410923a5a0e3
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-31 08:53:06 -0700

    Update rankings_endpoint.rs
    add rankings endpoints and structs

--- src/endpoints/rankings_endpoint.rs
+++ src/endpoints/rankings_endpoint.rs
@@ -7,4 +7,32 @@ use serde_json::{Value};
 use std::str::FromStr;
 use tqdm::tqdm;
 
-use crate::api_client::ApiClient; // Import the ApiClient from the parent module
+use crate::api_client::ApiClient; // Import the ApiClient from the parent module\
+
+//define the endpoint
+const RANKINGS_ENDPOINT: &str = "rankings";
+
+//define the params struct
+pub struct RankingsParams<'a> {
+    year: &'a str,
+    week: Option<&'a str>,
+    seasonType: Option<&'a str>, //like regular or postseason
+}
+
+pub struct RankingsResponse {
+}
+
+impl RankingsParams <'_> {
+    pub fn as_query_params(&self) -> Vec<(&str, &str)> {
+        let mut params = Vec::new();
+        params.push(("year", self.year));
+        // Add other fields if they exist in self
+        if let Some(week) = self.week {
+            params.push(("week", week));
+        }
+        if let Some(seasonType) = self.seasonType {
+            params.push(("seasonType", seasonType));
+        }
+        params
+    }
+}
