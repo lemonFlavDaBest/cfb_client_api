175f50a52e35fcd1d47def4c8bc3841c51fe2d3d
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-08-31 10:32:29 -0700

    Update ratings_endpoint.rs
    add the other endpoints

--- src/endpoints/ratings_endpoint.rs
+++ src/endpoints/ratings_endpoint.rs
@@ -9,3 +9,6 @@ use crate::api_client::ApiClient; // Import the ApiClient from the parent module
 
 //define the endpoint
 const RATINGS_ENDPOINT: &str = "ratings";
+const SP_ENDPOINT: &str = "ratings/sp";
+const SP_CONFERENCES_ENDPOINT: &str = "ratings/sp/conferences";
+const ELO_ENDPOINT: &str = "ratings/elo";
