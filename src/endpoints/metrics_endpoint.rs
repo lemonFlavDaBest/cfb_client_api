fd8be43b2d9b17b7e530ed6bb7848fcc64b7609a
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-09-01 07:09:37 -0700

    Update metrics_endpoint.rs
    add the rest of the endpoints

--- src/endpoints/metrics_endpoint.rs
+++ src/endpoints/metrics_endpoint.rs
@@ -11,3 +11,8 @@ use crate::api_client::ApiClient; // Import the ApiClient from the parent module
 const PPA_PREDICTED_ENDPOINT: &str = "ppa/predicted";
 const PPA_TEAMS_ENDPOINT: &str = "ppa/teams";
 const PPA_GAMES_ENDPOINT: &str = "ppa/games";
+const PPA_PLAYERS_GAMES_ENDPOINT: &str = "ppa/players/games";
+const PPA_PLAYERS_SEASON_ENDPOINT: &str = "ppa/players/season";
+const METRICS_WP_ENDPOINT: &str = "metrics/wp";
+const METRICS_WP_PREGAME_ENDPOINT: &str = "metrics/wp/pregame";
+
