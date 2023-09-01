aeedba8739699771829261443872450358ef73f5
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-09-01 09:43:42 -0700

    Update stats_endpoint.rs
    add all the ednpoints

--- src/endpoints/stats_endpoint.rs
+++ src/endpoints/stats_endpoint.rs
@@ -9,3 +9,7 @@ use crate::api_client::ApiClient; // Import the ApiClient from the parent module
 
 //define the endpoint
 const STATS_ENDPOINT: &str = "stats";
+const SEASON_ENDPOINT: &str = "stats/season";
+const SEASON_ADVANCED_ENDPOINT: &str = "stats/season/advanced";
+const GAME_ADVANCED_ENDPOINT: &str = "stats/game/advanced";
+const CATEGORIES_ENDPOINT: &str = "stats/categories";
