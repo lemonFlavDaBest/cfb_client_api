e2a3c0a13b759462ac9d079a41e1db668150b171
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-09-01 10:20:39 -0700

    Update draft_endpoint.rs
    add get draft positions fucntionality

--- src/endpoints/draft_endpoint.rs
+++ src/endpoints/draft_endpoint.rs
@@ -13,11 +13,22 @@ const TEAMS_ENDPOINT: &str = "draft/teams";
 const POSITIONS_ENDPOINT: &str = "draft/positions";
 const PICKS_ENDPOINT: &str = "draft/picks";
 
+pub struct DraftPicksParams<'a>{}
+
 #[derive(Deserialize, Debug)]
 pub struct DraftTeamsResponse{}
+
+#[derive(Deserialize, Debug)]
+pub struct DraftPositionsResponse{}
 
 pub async fn get_draft_teams(api_client: &ApiClient) -> Result<DraftTeamsResponse, Error> {
     let response = api_client.get_endpoint(TEAMS_ENDPOINT).await?;
     let json_response: DraftTeamsResponse = response.json().await?;
+    Ok(json_response)
+}
+
+pub async fn get_draft_positions(api_client: &ApiClient) -> Result<DraftPositionsResponse, Error> {
+    let response = api_client.get_endpoint(POSITIONS_ENDPOINT).await?;
+    let json_response: DraftPositionsResponse = response.json().await?;
     Ok(json_response)
 }
