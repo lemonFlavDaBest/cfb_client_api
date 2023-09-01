febb56378244e85949c72469d40b9a5b416057ec
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-09-01 07:12:06 -0700

    Update metrics_endpoint.rs
    add get predicted ppa functionality with struct included

--- src/endpoints/metrics_endpoint.rs
+++ src/endpoints/metrics_endpoint.rs
@@ -16,3 +16,26 @@ const PPA_PLAYERS_SEASON_ENDPOINT: &str = "ppa/players/season";
 const METRICS_WP_ENDPOINT: &str = "metrics/wp";
 const METRICS_WP_PREGAME_ENDPOINT: &str = "metrics/wp/pregame";
 
+pub struct PPAPredictedParams<'a> {
+    down: &'a str,
+    distance: &'a str,
+}
+
+impl PPAPredictedParams<'_> {
+    fn as_query_params(&self) -> Vec<(&str, &str)> {
+        let mut params = Vec::new();
+        // Add other fields if they exist in self
+        params.push(("down", self.down));
+        params.push(("distance", self.distance));
+        params
+    }
+}
+
+pub struct PPAPredictedResponse {}
+
+pub async fn get_ppa_predicted_with_params(api_client: &ApiClient, params: PPAPredictedParams<'_>) -> Result<PPAPredictedResponse, Error> {
+    let endpoint = PPA_PREDICTED_ENDPOINT;
+    let response = api_client.get_endpoint_with_params(endpoint, params.as_query_params()).await?;
+    let json_response: PPAPredictedResponse = response.json().await?;
+    Ok(json_response)
+}
