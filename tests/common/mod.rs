64802e966c6834831f07228ebf0a847c6ba7eb23
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-09-14 08:37:23 -0700

    Update mod.rs
    fix import. make sure to reutrn client

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,5 +1,7 @@
-use cfb_api::api_client::ApiClient;
+//bring ApiClient into scope
+use cfb_client_api::ApiClient;
 
 pub fn setup(api_key: &str) -> ApiClient {
     let api_client = ApiClient::new(api_key).unwrap();
+    api_client
 }
