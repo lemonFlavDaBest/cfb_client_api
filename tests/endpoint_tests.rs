cfc51da5b426109177dc76c07e268790e579e3d3
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-09-14 09:03:38 -0700

    Update endpoint_tests.rs
    add a test

--- tests/endpoint_tests.rs
+++ tests/endpoint_tests.rs
@@ -11,5 +11,10 @@ mod tests {
     static API_CLIENT: ApiClient = setup();
     
     #[test]
-
+    fn test_get_calendar() {
+        let start_year = 2016;
+        let end_year = 2017;
+        let response = get_calendar(&API_CLIENT, start_year, end_year).unwrap();
+        assert_eq!(response.len(), 2);
+    }
 }   
