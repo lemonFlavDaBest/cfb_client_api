ce73b6d5b960f6d72846b175563455b6234b0fe5
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-09-14 08:58:07 -0700

    Update mod.rs
    FIXING THIS FOR EASE

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,7 +1,8 @@
 //bring ApiClient into scope
 use cfb_client_api::ApiClient;
 
-pub fn setup(api_key: &str) -> ApiClient {
+pub fn setup() -> ApiClient {
+    let api_key = "test";
     let api_client = ApiClient::new(api_key).unwrap();
     api_client
 }
