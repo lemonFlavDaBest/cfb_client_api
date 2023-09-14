e514aefc8a43c55ce0176c0b46ab54c12715c280
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-09-14 09:19:35 -0700

    Update endpoint_tests.rs
    fixig this a little

--- tests/endpoint_tests.rs
+++ tests/endpoint_tests.rs
@@ -5,16 +5,15 @@ use common::setup;
 
 #[cfg(test)]
 mod tests {
-    use cfb_client_api::ApiClient;
+    use cfb_client_api::{ApiClient, calendar_endpoint::get_calendar};
 
     use super::*;
     static API_CLIENT: ApiClient = setup();
     
     #[test]
     fn test_get_calendar() {
-        let start_year = 2016;
-        let end_year = 2017;
-        let response = get_calendar(&API_CLIENT, start_year, end_year).unwrap();
+        let year = "2016";
+        let response = get_calendar(&API_CLIENT, year);
         assert_eq!(response.len(), 2);
     }
 }   
