a0d134fe4b97ae8aff4a9378fad660678e0c9f47
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-09-15 07:09:34 -0700

    Update endpoint_tests.rs
    adding tests and getting it setup

--- tests/endpoint_tests.rs
+++ tests/endpoint_tests.rs
@@ -3,17 +3,30 @@ mod common;
 
 use common::setup;
 
+
+
 #[cfg(test)]
 mod tests {
-    use cfb_client_api::{ApiClient, calendar_endpoint::get_calendar};
+
+    use cfb_client_api::{ApiClient, calendar_endpoint::get_calendar, };
 
     use super::*;
-    static API_CLIENT: ApiClient = setup();
+
+    static CLIENT: ApiClient = setup();
     
     #[test]
     fn test_get_calendar() {
+       
         let year = "2016";
-        let response = get_calendar(&API_CLIENT, year);
-        assert_eq!(response.len(), 2);
+        let response = get_calendar(&CLIENT, year);
+        println!("test_get_calendar");
+    }
+
+    #[test]
+    fn test_get_calendar_year_range() {
+        let start_year = 2016;
+        let end_year = 2017;
+        let response = get_calendar_year_range(&CLIENT, start_year, end_year);
+        println!("test_get_calendar_year_range");
     }
 }   
