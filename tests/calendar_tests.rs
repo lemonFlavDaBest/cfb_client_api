7826ba90713254baed6b262c68818268ecaca231
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-09-15 10:54:20 -0700

    Update calendar_tests.rs
    remove the testing part here because i am going to move this to a different file

--- tests/calendar_tests.rs
+++ tests/calendar_tests.rs
@@ -10,7 +10,6 @@ mod tests {
 
     use super::*;
 
-    
     #[tokio::test]
     async fn test_get_calendar() {
         let client = setup();
@@ -19,12 +18,4 @@ mod tests {
         assert!(response.is_ok());
     }
 
-    #[tokio::test]
-    async fn test_get_calendar_year_range() {
-        let client = setup();
-        let start_year = 2016;
-        let end_year = 2017;
-        //let response = get_calendar_year_range(&CLIENT, start_year, end_year);
-        println!("test_get_calendar_year_range");
-    }
 }   
